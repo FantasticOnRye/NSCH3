f8d3a44ab33c520264ac17d16382b114bb792f1b
JeroldB26 <156853463+JeroldB26@users.noreply.github.com>
2026-01-31 20:12:45 -0500

    new map

--- Mapdata.jsx
+++ Mapdata.jsx
@@ -8,15 +8,15 @@ export default function MapScreen() {
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: 40.4433,
-          longitude: -79.9436,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
+          latitude: 42.7299,       // RPI latitude
+          longitude: -73.6795,     // RPI longitude
+          latitudeDelta: 0.02,     // zoom in closer (~small campus)
+          longitudeDelta: 0.01,
         }}
       >
         <Marker
-          coordinate={{ latitude: 40.4433, longitude: -79.9436 }}
-          title="Sample Location"
+          coordinate={{ latitude: 42.7345670, longitude: -73.6763294 }}
+          title="Mumu Cafe"
         />
       </MapView>
     </View>
